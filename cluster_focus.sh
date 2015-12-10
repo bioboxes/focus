4c2b9f8ea68c4f430a2de79b3cd3ef9090bd2a41
Stefan Janssen <sjanssen@bioinf009.helmholtz-hzi.de>
2015-12-10 16:12:28 +0100

    restrictred the number of CPUs for a docker run

---
+++ cluster_focus.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+memusage=50; #in gigabyte
+ncores=4; #how many CPU cores can be addressed by the container
+#$ -S /usr/bin/bash
+#$ -N focus
+#$ -e /vol/projects/sjanssen/dockerruns/ERR/
+#$ -o /vol/projects/sjanssen/dockerruns/OUT/
+#$ -pe multislot 4
+#$ -l virtual_free=50g
+#$ -l mem_free=50g
+#$ -cwd
+
+cd /vol/projects/sjanssen/docker_profiling_tools
+docker build -f motu/Dockerfile_focus  -t focus .
+docker run --rm=true --memory=${memusage}g --memory-swap=-1 --cpuset-cpus=${ncores} \
+-v "/vol/projects/sjanssen/CAMI/:/exchange/input" \
+-v "/vol/projects/sjanssen/dockerruns/focus:/exchange/output:rw" \
+-t focus
